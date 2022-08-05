fd43d357facd47c393d0555536aab90c2cfb9171
Komal Killedar <komal.killedar@corp.ola.com>
2022-08-05 14:08:50 +0530

    done with object

---
+++ obj.js
@@ -0,0 +1,8 @@
+var user = {
+    name: "Komal",
+    age: 26,
+    city: "Pune",
+    hobbies: 10
+};
+user.name = "Komal Killedar";
+console.log(user);
--- obj.ts
+++ obj.ts
@@ -1,7 +1,16 @@
-let user = {
-    name:"Komal",
-    age:26,
-    city:"Pune"
+interface usersType{
+    name:String,
+    age:number,
+    city:String
+    hobbies:any
 }
 
+let user:usersType = {
+    name:"Komal",
+    age:26,
+    city:"Pune",
+    hobbies:10
+}
+user.name="Komal Killedar";
 console.log(user);
+
