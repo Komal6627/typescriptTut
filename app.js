2f811ca7d2f1658e44676473c80c2d3897fa9891
Komal Killedar <komal.killedar@corp.ola.com>
2022-08-05 12:04:29 +0530

    done with types of typescript

---
+++ app.js
@@ -0,0 +1,24 @@
+// let a  = "Komal";
+// console.log(a);
+// export {}
+// class App{
+//     test(){
+//         console.log("This is class app with new func");   
+//     }
+// }
+// let a1 = new App();
+// a1.test();
+//Types,Number,String
+// let a:number =10;
+// console.log(a);
+var a = 10.10;
+console.log(a);
+//error
+// let a:string =10;
+// console.log(a);
+//String
+// let a:string="Hello";
+// console.log(a);
+//Boolean
+// let a:boolean=true;
+// console.log(a);
