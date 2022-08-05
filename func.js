66b9791585ac98e69fb9011ebbc44aec20d5c286
Komal Killedar <komal.killedar@corp.ola.com>
2022-08-05 15:13:24 +0530

    done with function

---
+++ func.js
@@ -0,0 +1,8 @@
+function cal() {
+    return 10 + 10;
+}
+console.log(cal());
+function hello() {
+    return "Hello";
+}
+console.log(hello());
---
+++ func.ts
@@ -0,0 +1,11 @@
+function cal():number {
+    return 10 +10
+}
+
+console.log(cal());
+
+function hello():string {
+    return "Hello"
+}
+console.log(hello());
+
